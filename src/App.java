c875dee08556b39a9983761456da7f5051422b07
Bech1337 <sebberas2@gmail.com>
2022-09-05 15:24:28 +0200

    initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        final Scanner scanner = new Scanner(System.in);
+
+        Terminal terminal = new Terminal(scanner);
+        while (true) {
+            // TODO(Bech): ...
+        }
+    }
+}
---
+++ src/Terminal.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Terminal {
+    public Terminal(Scanner scanner) {
+        
+    }
+}
